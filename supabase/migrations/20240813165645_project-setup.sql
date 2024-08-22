614640fe07b950053fff7d2eff5cc42551e936a7
Shoaib Burq <saburq@gmail.com>
2024-08-22 17:27:22 +0200

    update policies to select foldername based on userid

--- supabase/migrations/20240813165645_project-setup.sql
+++ supabase/migrations/20240813165645_project-setup.sql
@@ -261,14 +261,8 @@ FOR EACH ROW EXECUTE FUNCTION public.update_project_bounds();
 -- Avatar bucket policy
 --
 
-CREATE POLICY "Give anon users access to avatars bucket" ON storage.objects
-FOR SELECT TO public USING (bucket_id = 'avatars' AND auth.role() = 'anon');
-
-CREATE POLICY "Allow users to upload avatars" ON storage.objects
-FOR INSERT TO authenticated WITH CHECK (bucket_id = 'avatars' AND auth.role() = 'authenticated');
-
-CREATE POLICY "Allow users to delete their avatar folder" ON storage.objects
-FOR DELETE TO authenticated USING (bucket_id = 'avatars' AND (select auth.uid()) = owner);
-
-CREATE POLICY "Allow users to update their avatar folder" ON storage.objects
-FOR UPDATE TO authenticated USING (bucket_id = 'avatars' AND (select auth.uid()) = owner);
+CREATE POLICY "Give anon users access to avatars bucket" ON storage.objects FOR SELECT TO public USING (bucket_id = 'avatars' AND auth.role() = 'anon');
+CREATE POLICY "Give users select access to own folder in avatars bucket" ON storage.objects FOR SELECT TO authenticated USING (bucket_id = 'avatars' AND (select auth.uid()::text) = (storage.foldername(name))[1]);
+CREATE POLICY "Give users insert access to own folder in avatars bucket" ON storage.objects FOR INSERT TO authenticated WITH CHECK (bucket_id = 'avatars' AND (select auth.uid()::text) = (storage.foldername(name))[1]);
+CREATE POLICY "Give users delete access to own folder in avatars bucket" ON storage.objects FOR DELETE TO authenticated USING (bucket_id = 'avatars' AND (select auth.uid()::text) = (storage.foldername(name))[1]);
+CREATE POLICY "Give users update access to own folder in avatars bucket" ON storage.objects FOR UPDATE TO authenticated USING (bucket_id = 'avatars' AND (select auth.uid()::text) = (storage.foldername(name))[1]);
